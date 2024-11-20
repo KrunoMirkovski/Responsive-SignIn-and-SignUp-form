4b3955ef73ae76ea7702d62eb014a78515625f94
KRUNISLAV MIRKOVSKI <kmirkovski@gmial.com>
2024-11-20 03:56:32 +0100

    adding Media Queries

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+//The JavaScript toggles the active class on the .container element to switch between sign-in and sign-up forms.
+
 const container = document.querySelector(".container");
 const registerBtn = document.querySelector(".register_btn");
 const loginBtn = document.querySelector(".login_btn");
 
+// Add event listener to the Register button
 registerBtn.addEventListener("click", () => {
   container.classList.add("active");
 });
 
+// Add event listener to the Log In button
 loginBtn.addEventListener("click", () => {
   container.classList.remove("active");
 });
